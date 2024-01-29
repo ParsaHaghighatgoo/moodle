c9822696af862388be52bd08d1f9cd6f71eb0ad4
Siralexali <alisharafiyan1@gmail.com>
2024-01-29 19:52:50 +0330

    nemidonmmmmmmmmmmmmmmmmm

--- Moodle/src/main/java/com/example/moodle/Examing.java
+++ Moodle/src/main/java/com/example/moodle/Examing.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
@@ -16,7 +17,16 @@ import java.io.IOException;
 
 public class Examing {
 
-
+    @FXML
+    private ButtonBar buttonBar;
+    @FXML
+    private Button prev;
+    @FXML
+    private Button finish;
+    @FXML
+    private Button cancel2;
+    @FXML
+    private AnchorPane finish_page;
     @FXML
     private TextArea descriptiveAnswer;
 
@@ -59,6 +69,10 @@ public class Examing {
         counter = 0;
         Question question = quiz.questions.get(counter);
         if (question.getQtype().equals(QuestionType.TEST)){
+            test.setVisible(true);
+            descriptive.setVisible(false);
+            finish_page.setVisible(false);
+            buttonBar.setVisible(true);
             testQuestion.setText(question.getQuestionTest());
             tcheckA.setText(question.getOption1());
             tcheckB.setText(question.getOption2());
@@ -68,6 +82,8 @@ public class Examing {
         } else {
             test.setVisible(false);
             descriptive.setVisible(true);
+            finish_page.setVisible(false);
+            buttonBar.setVisible(true);
             descriptiveQuestion.setText(question.getQuestionDes());
             counter++;
         }
@@ -76,7 +92,11 @@ public class Examing {
     @FXML
     private void show_questions(){
         Question question = quiz.questions.get(counter);
-        if (question.getQtype().equals(QuestionType.TEST)){
+        if (question.getQtype().equals(QuestionType.TEST) && counter != quiz.questions.size()-1){
+            test.setVisible(true);
+            descriptive.setVisible(false);
+            finish_page.setVisible(false);
+            buttonBar.setVisible(true);
             testQuestion.setText(question.getQuestionTest());
             tcheckA.setText(question.getOption1());
             tcheckB.setText(question.getOption2());
@@ -87,11 +107,18 @@ public class Examing {
             tcheckB.setSelected(false);
             tcheckC.setSelected(false);
             tcheckD.setSelected(false);
-        } else {
+        } else if (counter != quiz.questions.size()-1 && question.getQtype().equals(QuestionType.DESCRIPTIVE)){
             test.setVisible(false);
             descriptive.setVisible(true);
+            finish_page.setVisible(false);
+            buttonBar.setVisible(true);
             descriptiveQuestion.setText(question.getQuestionDes());
             counter++;
+        } else {
+            test.setVisible(false);
+            descriptive.setVisible(false);
+            finish_page.setVisible(true);
+            buttonBar.setVisible(true);
         }
     }
 
@@ -101,12 +128,30 @@ public class Examing {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Signup.class.getResource("userPage.fxml"));
             Parent root = fxmlLoader.load();
-            UserPage userPage = fxmlLoader.getController();
             currentStage.setScene(new Scene(root));
             currentStage.setTitle("User Page");
             currentStage.show();
         } catch (IOException e) {
             System.err.println("Error loading the user page: " + e.getMessage());
         }
+    }
+
+    @FXML
+    private void handle_cancel_button(){
+        setQuiz(quiz);
+    }
+
+    @FXML
+    private void handle_finish_button(){
+        test.setVisible(false);
+        descriptive.setVisible(false);
+        finish_page.setVisible(true);
+        buttonBar.setVisible(false);
+    }
+
+    @FXML
+    private void handle_prev_button(){
+        counter -= 2;
+        show_questions();
     }
 }
